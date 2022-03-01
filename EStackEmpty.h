1dabce94546832f4623659e0ee7a70143ee83ec7
Danabek Bisenbenov <diazunititled@gmail.com>
2022-03-01 23:54:49 +0700

    Added "EStackEmpty" & "EStackException" classes.
    Classes that throw a special exception when the stack is empty.

---
+++ EStackEmpty.h
@@ -0,0 +1,25 @@
+#ifndef ESTACKEMPTY_H
+#define ESTACKEMPTY_H
+#include "EStackException.h"
+
+class EStackEmpty : public EStackException
+{
+public:
+    //конструктор
+    EStackEmpty() : EStackException("Unfortunately, your stack is empty.")
+    {
+
+    }
+
+    //конструктор копирования
+    EStackEmpty(const EStackEmpty &object) : EStackException(object)
+    {
+
+    }
+
+    //~EStackEmpty();
+
+
+};
+
+#endif // ESTACKEMPTY_H
---
+++ EStackException.h
@@ -0,0 +1,41 @@
+#ifndef ESTACKEXCEPTION_H
+#define ESTACKEXCEPTION_H
+#include <cstring>
+
+class EStackException
+{
+private:
+    char* _message;
+
+public:
+    //конструктор
+    EStackException(const char* message)
+    {
+        //выделяем память под сообщение
+        _message = new char[strlen(message) + 1];
+        //сохраняем данные об ощибке
+        strcpy(_message, message);
+    };
+
+    //конструктор копирования
+    EStackException(const EStackException &object)
+    {
+        _message = new char[strlen(_message) + 1];
+        strcpy(_message, object._message);
+    }
+
+    //Деструктор (освободжаем выделенную память)
+    ~EStackException()
+    {
+        delete _message;
+    };
+
+    //Возвращаем диагностическое сообщение
+    const char* what() const
+    {
+        return _message;
+    }
+
+};
+
+#endif // ESTACKEXCEPTION_H

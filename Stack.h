a58c84fae21eca22971f80dd800e1534d5bc6fec
Danabek Bisenbenov <diazunititled@gmail.com>
2022-03-02 20:58:36 +0700

    Add stack's methods and main class.
    Added methods such as Push, Pop, Clear, Size. Also added a destructor.
    In the main class  has pushed some integer elements onto the stack and returned its size.

---
+++ Stack.h
@@ -0,0 +1,71 @@
+#ifndef STACK_H
+#define STACK_H
+#include <iostream>
+#include <functional>
+#include <EStackEmpty.h>
+#include <EStackException.h>
+
+//Стек опишем как шаблонный класс на основе односвязного списка
+template<class T>
+class Stack
+{
+private:
+    struct NodeStack //Узел описываем структурой
+    {
+        const T item_;
+        NodeStack *pointPrevious_; //Указатель на предыдущее звено стека
+    };
+    NodeStack *pointBottom_; // Указатель на последний элемент стека
+    int size_ = 0;
+
+
+public:
+    ~Stack() {Clear();} //Деструктор
+
+    void Clear() // Удаление вслех узлов
+    {
+        while(pointBottom_ != nullptr) //Пока последний элемент не нулевой, то есть пока в стеке все ещё есть элементы
+        {
+            NodeStack *tmp = pointBottom_;
+            pointBottom_ = pointBottom_->pointPrevious_;
+            delete tmp;
+        }
+
+    }
+
+    void Push(const T &item) //Кладем значение в стек
+    {
+        NodeStack *node = new NodeStack{item, pointBottom_}; //Создаём новое звено
+
+        if(node == nullptr)
+        {
+            throw EStackException("No memory!");
+        }
+
+        pointBottom_ = node; //Делаем новое звено последним
+        size_++; //Увеличиваем размер стека
+    }
+
+    const T Pop() //Извлекаем значение из стека
+    {
+        if(pointBottom_ == nullptr) //Если стек пустой, выбрасываем исключение
+        {
+            throw EStackEmpty();
+        }
+
+        const T item = pointBottom_->item_; //Сохраняем значение
+        NodeStack *tmp = pointBottom_;
+        pointBottom_ = pointBottom_->pointPrevious_; //Конец стека теперь на предпоследнем элементе
+        delete tmp; //Удаляем последнее звено
+        size_--; //Уменьшаем размер стека
+        return item; //Возвращаем новое значение
+    }
+
+    int Size()
+    {
+        return size_;
+    }
+
+};
+
+#endif // STACK_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <EStackEmpty.h>
+#include <EStackException.h>
+#include <Stack.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
 
-    return a.exec();
+    //return a.exec();
+
+    //Тестим
+    Stack<int> Stack;
+    Stack.Push(2);
+    Stack.Push(3);
+    Stack.Push(4);
+    Stack.Push(5);
+
+    std::cout << Stack.Size() << std::endl;
+
+    return 1;
 }

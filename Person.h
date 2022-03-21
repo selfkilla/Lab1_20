6cb7062fc9408b96ef10fbbe0e03561fa6d59855
Danabek Bisenbenov <diazunititled@gmail.com>
2022-03-21 13:04:00 +0700

    Added Person class.

---
+++ Person.h
@@ -0,0 +1,77 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <iostream>
+class Person
+{
+private:
+    //long key;
+    std::string last_name;
+    std::string first_name;
+    std::string patronimic;
+public:
+    //Default constructor
+    Person() {}
+
+    //Constructor
+    Person(std::string firstName, std::string lastName, std::string patronimic_)
+    {
+        first_name = firstName;
+        last_name = lastName;
+        patronimic = patronimic_;
+    }
+
+    //Copy constructor
+    Person(const Person& person)
+    {
+        last_name = person.getLastName();
+        first_name = person.getFirstName();
+        patronimic = person.getPatronimic();
+    }
+
+    Person &operator= (Person const& arg)
+    {
+        if(this != &arg)
+        {
+            first_name = arg.getFirstName();
+            last_name = arg.getLastName();
+            patronimic = arg.getPatronimic();
+        }
+
+        return *this;
+    }
+
+    //Destructor
+    ~Person() {}
+
+    //Get full name of a person
+    std::string getFullName()
+    {
+        std::string fullName;
+        fullName.append(last_name).append(1, ' ')
+                .append(first_name).append(1, ' ')
+                .append(patronimic);
+        return fullName;
+    }
+
+    const std::string getLastName() const { return last_name; }
+
+    const std::string getFirstName() const { return first_name; }
+
+    const std::string getPatronimic() const { return patronimic; }
+
+    void setLastName(std::string namePart)
+    {
+        last_name = namePart;
+    }
+
+    void setFirstName(std::string namePart)
+    {
+        first_name = namePart;
+    }
+
+    void setPatronimic(std::string namePart)
+    {
+        patronimic = namePart;
+    }
+};
+#endif // PERSON_H
